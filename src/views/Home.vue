36c6e1b8e78a7fa89afad4cf2b9fa8e6da511555
Tzuhui <angel1996421@gmail.com>
2020-12-10 12:14:46 +0800

    git init

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Tzuhui's Github Actions Deploy For Vue CLI"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
