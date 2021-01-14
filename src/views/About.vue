5ce055d22e8aa8b89789ffe7f1e62b30091caf95
Tzuhui <angel1996421@gmail.com>
2021-01-14 14:26:31 +0800

    add env set title (test ci env)

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    {{ title }}
   </div>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        title: process.env.VUE_APP_TITLE,
+      }
+    }
+  }
+</script>
